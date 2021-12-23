4ff76e45800f103ef9e9ada6a56a75fbe5f68ec9
Yukams <valentin.tahon.pro@gmail.com>
2021-12-23 18:20:40 +0100

    fix : dbUser with wrong parameters

--- ProjetS5/src/back/dbobjects/DbUser.java
+++ ProjetS5/src/back/dbobjects/DbUser.java
@@ -3,9 +3,7 @@ package back.dbobjects;
 public class DbUser {
     public int id;
     public String username;
-    public String nom;
-    public String prenom;
-    public int groupeType;
-
-    public DbUser() {}
+    public String name;
+    public String surname;
+    public int groupType;
 }
