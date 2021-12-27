58e5f9c289d07226a8bdae710dfb68f51e251a24
Marrmud <38955611+Marrmud@users.noreply.github.com>
2021-12-27 17:42:41 +0100

    ServerInterface

--- ProjetS5/src/front/server/ServerInterface.java
+++ ProjetS5/src/front/server/ServerInterface.java
@@ -19,7 +19,7 @@ public class ServerInterface extends javax.swing.JFrame {
     protected String[] groups;
     private final String connected = "Connected";
     private final String disconnected = "Disconnected";
-    /*Cx*/
+
     private RootRequest rootRequest;
 
     /**
